1e41aac958ccc8e800be037eebae632498bfc9ca
giamaf <gianluca.maffucci@hotmail.com>
2024-01-29 15:03:28 +0100

    added axios' endpoint + vue's object and data

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 console.log('JS OK');
 
+// Importo l'endpoint boolean per ottenere gli indirizzi mail
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+// Importo l'oggetto Vue
+const { createApp } = Vue;
+
+const myApp = createApp({
+    name: 'Vue Email List',
+    data: () => ({
+        title: 'Vue Email List',
+        emails: [],
+        times: 10,
+    }),
+
+})
+
+myApp.mount('#root');
